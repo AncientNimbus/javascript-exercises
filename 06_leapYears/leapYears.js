8276cd42261689905f81d62cc828251db0468b87
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-01 17:52:35 +0100

    feat: Complete JS exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+/**
+ *
+ * @param {number} year
+ * @returns {boolean}
+ */
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
