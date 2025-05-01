644fb78141b98178b5d47cb386e6d47baa5dd0f7
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-01 16:28:26 +0100

    feat: Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const { isNumberObject } = require("util/types");
 
+/**
+ * Sum of every integer between two numbers
+ * @param {Number} int1
+ * @param {Number} int2
+ * @returns {Number}
+ */
+const sumAll = function (int1, int2) {
+  let conditions =
+    int1 + int2 > 0 &&
+    typeof int1 === "number" &&
+    typeof int2 === "number" &&
+    int1 === Math.round(int1) &&
+    int2 === Math.round(int2);
+
+  if (conditions) {
+    let result = 0;
+    for (let i = int1; i <= int2; i++) {
+      result += i;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
