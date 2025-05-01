910d76b1b5b5983ab32bb35b668ce82bd7a93a2d
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-01 18:11:17 +0100

    feat: Complete JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+/**
+ * Convert temperatures from Fahrenheit to Celsius
+ * @param {number} f Degree Fahrenheit
+ * @returns {number}
+ */
+const convertToCelsius = function (f) {
+  let c = Math.round((((f - 32) * 5) / 9) * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+/**
+ * Convert temperatures from Celsius to Fahrenheit
+ * @param {*} c Degree Celsius
+ * @returns {number}
+ */
+const convertToFahrenheit = function (c) {
+  let f = Math.round(((c * 9) / 5 + 32) * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
