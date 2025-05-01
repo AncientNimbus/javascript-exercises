26a0e9ccb4a5de1de61bb7280db4408c30751f35
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-01 14:55:39 +0100

    feat: Complete JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+/**
+ *
+ * @param {string} str
+ * @returns {string}
+ */
+const reverseString = function (str) {
+  let reversedStr = [];
+  for (let char = str.length; char > 0; char--) {
+    reversedStr.push(str[char - 1]);
+  }
+  return reversedStr.join("");
 };
 
 // Do not edit below this line
