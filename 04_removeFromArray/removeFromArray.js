ab3b1746cb55382838a568d0e88af2d3e9c60c4f
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-01 15:16:48 +0100

    refactor: Update JS exercise 04 logic
    Current iteration can handle multiple of the same value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+/**
+ *
+ * @param {Array} arr
+ * @param {number} num
+ */
+const removeFromArray = function (arr, num) {
+  let target = arr.indexOf(num);
+  while (target !== -1) {
+    arr.splice(target, 1);
+    target = arr.indexOf(num);
+  }
+  //   if (target !== -1) {
+  //     arr.splice(target, 1);
+  //   }
+  console.log(arr);
+  return arr;
 };
+
+removeFromArray([1, 2, 2, 4], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
