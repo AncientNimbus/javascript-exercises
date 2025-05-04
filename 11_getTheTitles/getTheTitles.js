7884c9dcb28b508963fb7facb90965a97641bcfe
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-04 18:46:08 +0100

    feat: Complete JS exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
-
+/**
+ *
+ * @param {Array} bookObj
+ */
+const getTheTitles = function (booksArr) {
+  return booksArr.map((book) => book.title);
+  //   let bookTitle = [];
+  //   booksObj.forEach((book) => {
+  //     bookTitle.push(book.title);
+  //   });
+  //   return bookTitle;
 };
 
 // Do not edit below this line
