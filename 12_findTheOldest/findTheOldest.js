b410bb128a7b485c8a152f40452fb8aa1e2a3f31
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-04 19:31:19 +0100

    feat: Complete JS exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+/**
+ *
+ * @param {Array} peopleArr
+ */
+const findTheOldest = function (peopleArr) {
+  const currentYear = new Date().getFullYear();
+  const age = (dob, dod = currentYear) => dod - dob;
 
+  peopleArr.forEach((person) => {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = currentYear;
+    }
+  });
+
+  peopleArr.sort(
+    (a, b) =>
+      age(a.yearOfBirth, a.yearOfDeath) - age(b.yearOfBirth, b.yearOfDeath)
+  );
+
+  return peopleArr.at(-1);
 };
 
 // Do not edit below this line
