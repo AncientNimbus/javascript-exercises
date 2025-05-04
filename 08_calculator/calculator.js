7c410c7a16264340849ad2d60969a48acdaf5bb3
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-04 15:52:18 +0100

    feat: Complete JS exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+/**
+ * Calculate the sum of any number of numeric arguments
+ * @param  {...number} numbers
+ * @returns {number} The total sum of the provided numbers
+ */
+const add = function (...numbers) {
+  return numbers.reduce((acc, num) => acc + num, 0);
 };
 
-const subtract = function() {
-	
+/**
+ * Calculate the difference of two numbers
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+/**
+ * Calculate the sum of all numbers in an array.
+ * @param {number[]} arr - An array of numbers
+ * @returns {number}
+ */
+const sum = function (arr) {
+  return arr.reduce((arr, num) => arr + num, 0);
 };
 
-const multiply = function() {
-
+/**
+ * Calculate the product of all numbers in an array
+ * @param {number[]} arr - An array of numbers
+ * @returns {number}
+ */
+const multiply = function (arr) {
+  return arr.reduce((arr, num) => arr * num, 1);
 };
 
-const power = function() {
-	
+/**
+ * Calculate the power of two numbers
+ * @param {number} x
+ * @param {number} n
+ * @returns {number}
+ */
+const power = function (x, n) {
+  return x ** n;
 };
 
-const factorial = function() {
-	
+/**
+ * Calculate the factorial of a number
+ * @param {number} n
+ * @returns {number}
+ */
+const factorial = function (n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +65,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
