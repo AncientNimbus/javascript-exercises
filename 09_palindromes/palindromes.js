c3c043d6492be1fd6a90634502910df992e1fbc0
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-04 16:37:33 +0100

    feat: Complete JS exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+/**
+ *
+ * @param {string} str
+ */
+const palindromes = function (str) {
+  const regex = /\W/g;
+  const chars = str.replaceAll(regex, "").toLowerCase().split("");
+  const reversedChar = chars.toReversed();
 
+  return chars.every((char, index) => char === reversedChar[index]);
 };
 
 // Do not edit below this line
